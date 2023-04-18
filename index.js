07654ccaabb4ce83294e6e440a02529c2e62ad44
Caleb <cackmed@gmail.com>
2023-04-18 03:55:11 -0700

    put together a simple chatbot in the terminal using chat gpt

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 require('dotenv').config();
 const { Configuration, OpenAIApi } = require('openai');
+const readline  = require("readline");
 
 const openai = new OpenAIApi(new Configuration({
     apiKey: process.env.SECRET_KEY
 }))
 
-openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{
-        role: "user",
-        content: "Hello ChatGPT"
-    }]
-}).then(res => {
-    console.log(res)
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 })
 
+userInterface.prompt()
+
+
+userInterface.on('line', async input => {
+    const res = openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{
+            role: "user",
+            content: input
+        }]
+    })
+    
+    console.log(res.data.choices[0].message.content);
+    userInterface.prompt()
+})
 
 
